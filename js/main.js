ed42d125e806b7a31272f3c54eddf339d7718e0c
devDandy <dschnd@gmail.com>
2019-10-25 14:27:41 -0500

    Added navigation

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("navbar").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("navbar").style.width = "0%";
+  } 
+
