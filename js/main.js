bd8da575b1a8e10feeee107b22f6c267b111398e
devDandy <dschnd@gmail.com>
2019-11-04 16:07:35 -0600

    Added back to top button

--- js/main.js
+++ js/main.js
@@ -8,10 +8,22 @@ function openNav() {
     document.getElementById("navbar").style.width = "0%";
   } 
 
+var backToTopButton = document.getElementById("backToTop");
 
-  function showPortfolioInformation() 
-  {
 
+// When viewer scrolls make the button viewable
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
   }
+}
 
-
+// When clicked go back to top
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
